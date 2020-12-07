fd9f0e1a4a8ebd12e6043e87594cfe8bb1887913
Nathan West <Lucretiel@gmail.com>
2020-12-07 16:05:22 -0500

    Remove accidentally included code; bump to 2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+## 2.0.0
+
+### Removed
+
+- Removed `IndentedWrite`, which was left over from a previous (unreleased) version of `indent-write` which I simply forgot to delete from the code before releasing
+
+## 1.0.0
+
+Initial release!
--- src/fmt.rs
+++ src/fmt.rs
@@ -1,48 +1,4 @@
 use std::fmt;
-
-#[derive(Debug, Clone)]
-pub struct IndentedWrite<'a, W> {
-    writer: W,
-    prefix: &'a str,
-
-    // True if we need to insert an indent before our next write
-    insert_indent: bool,
-}
-
-impl<'a, W> IndentedWrite<'a, W> {
-    pub fn dedent(self) -> W {
-        self.writer
-    }
-}
-
-impl<'a, W: fmt::Write> fmt::Write for IndentedWrite<'a, W> {
-    fn write_str(&mut self, mut buf: &str) -> Result<(), fmt::Error> {
-        // TODO: this is a highly stateful algorithm. Make sure it's panic-safe
-        // against self.writer.write_str
-
-        while !buf.is_empty() {
-            if self.insert_indent {
-                self.writer.write_str(self.prefix)?;
-                self.insert_indent = false;
-            }
-
-            // This +1 is safe because string lengths must fit in a usize, so
-            // the index of the newline character is necessarily less than USIZE_MAX
-            match buf.find('\n').map(|idx| idx + 1) {
-                None => return self.writer.write_str(buf),
-                Some(newline_boundary) => {
-                    self.writer
-                        .write_str(unsafe { buf.get_unchecked(..newline_boundary) })?;
-                    self.insert_indent = true;
-                    buf = unsafe { buf.get_unchecked(newline_boundary..) };
-                }
-            }
-        }
-
-        Ok(())
-    }
-}
-
 /// Adapter for writers to indent each line
 ///
 /// An `IndentWriter` adapts a [`fmt::Write`] object to insert an indent before
